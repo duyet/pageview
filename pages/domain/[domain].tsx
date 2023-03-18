e9ae3108e14a95068daece37908d0fd0bcf9d93b
Duyet Le <duyet.cs@gmail.com>
2023-03-18 22:59:30 +0700

    feat: update domain page

--- pages/domain/[domain].tsx
+++ pages/domain/[domain].tsx
@@ -1,8 +1,8 @@
 import type { GetServerSideProps } from 'next'
 import Link from 'next/link'
-import { Card, Flex, Grid, Col, Button } from '@tremor/react'
-import { List, ListItem, Badge, Title } from '@tremor/react'
-import { ArrowNarrowLeftIcon, ExternalLinkIcon } from '@heroicons/react/solid'
+import { Card, Flex, Button, Divider, Title } from '@tremor/react'
+import { BarList, Text, Bold } from '@tremor/react'
+import { ArrowNarrowLeftIcon } from '@heroicons/react/solid'
 
 import prisma from '../../lib/prisma'
 
@@ -36,37 +36,31 @@ export default function Home({ domain, urlStats }: URLStatsProps) {
           </Button>
         </Link>
       </Title>
-      <List>
-        {urlStats.map((row: any) => {
-          return (
-            <ListItem key={row.url}>
-              <Grid numCols={1} numColsSm={4} className="mt-6">
-                <Col>
-                  <div>
-                    <Flex className="space-x-5" alignItems="start">
-                      <Link href={`/url/${row.id}`}>{row.url}</Link>
-                      <Badge>PageView: {row._count.pageViews}</Badge>
 
-                      <Link href={row.url} target="_blank">
-                        <Badge>
-                          <Button
-                            size="sm"
-                            variant="light"
-                            icon={ExternalLinkIcon}
-                            iconPosition="right"
-                          >
-                            Visit URL
-                          </Button>
-                        </Badge>
-                      </Link>
-                    </Flex>
-                  </div>
-                </Col>
-              </Grid>
-            </ListItem>
-          )
+      <Text>Total URL: {urlStats.length}</Text>
+
+      <Divider />
+
+      <Flex className="mt-4">
+        <Text>
+          <Bold>URL</Bold>
+        </Text>
+        <Text>
+          <Bold>PageView</Bold>
+        </Text>
+      </Flex>
+
+      <BarList
+        className="mt-2"
+        data={urlStats.map((row: any) => {
+          return {
+            name: row.url,
+            value: row._count.pageViews,
+            href: `/url/${row.id}`,
+            target: '_self',
+          }
         })}
-      </List>
+      />
     </Card>
   )
 }
@@ -86,6 +80,9 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
       id: true,
       url: true,
       _count: true,
+    },
+    orderBy: {
+      pageViews: { _count: 'desc' },
     },
   })
 
